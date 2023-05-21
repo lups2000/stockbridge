d4ab5d6c03b5aaba84af8ada8bbd2d734371e05e
Mohamed Besrour <ge49gej@mytum.de>
2023-05-21 14:04:46 +0200

    repo setup

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { UserInfo } from "./pages/UserInfo";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/userInfo" element={<UserInfo />} />
+        </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import homepageImage from "../assets/homepageImage.png";
+export function Home() {
+  return (
+    <div>
+      <img style={{ maxWidth: "100%" }} src={homepageImage} alt="homepage" />
+    </div>
+  );
+}
