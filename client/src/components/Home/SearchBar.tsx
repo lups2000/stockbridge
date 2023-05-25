8d127393e77136c8369620150daceac949f89e6b
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-05-25 19:12:08 +0200

    first prototype searchbar + hook for media queries

--- client/src/components/Home/SearchBar.tsx
+++ client/src/components/Home/SearchBar.tsx
@@ -1,12 +1,56 @@
+import { Button, Dropdown } from "react-bootstrap";
+import { palette } from "../../utils/colors";
+import useMediaQuery from "../../hooks/useMediaQuery";
+
 /**
  * Component that will manage the search of an advert...
  */
 export function SearchBar() {
+  const matches = useMediaQuery("(min-width: 768px)");
   return (
-    <input
-      style={{ width: "20%", maxWidth: 260 }}
-      type="text"
-      placeholder="Search here"
-    />
+    <div
+      style={{
+        backgroundColor: "white",
+        borderRadius: 30,
+        paddingLeft: 15,
+      }}
+    >
+      <div
+        style={{
+          margin: 3,
+          display: "flex",
+          flexDirection: matches ? "row" : "column",
+          gap: 20,
+        }}
+      >
+        <input
+          style={{ border: "none", width: 250 }}
+          placeholder="What are you searching for?"
+        ></input>
+        <input style={{ border: "none" }} placeholder="Location"></input>
+        <Dropdown>
+          <Dropdown.Toggle
+            style={{ backgroundColor: "white", border: "none", color: "gray" }}
+            id="dropdown-basic"
+          >
+            Categories
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Flowers</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Food</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Other</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Button
+          style={{
+            border: "none",
+            backgroundColor: palette.subSectionsBgAccent,
+            borderRadius: 30,
+          }}
+        >
+          Search
+        </Button>
+      </div>
+    </div>
   );
 }
