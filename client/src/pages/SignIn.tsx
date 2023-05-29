d3dcab47d66827972a40f68db56a4a68e3759eba
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-05-29 09:46:42 +0200

    adjust sign in and signup pages and add second form(signup)

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -2,48 +2,60 @@ import { FC } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Title } from "../components/Text/Title";
 import { palette } from "../utils/colors";
-import signUpImage from "../assets/signUp.png";
+//import signUpImage from "../assets/signUp.png";
 import { BodyText } from "../components/Text/BodyText";
 import backIcon from "../assets/back.svg";
 import { useNavigate } from "react-router-dom";
+import useMediaQuery from "../hooks/useMediaQuery";
 export const SignIn: FC = () => {
   const navigate = useNavigate();
+  const matches = useMediaQuery("(min-width: 768px)");
   return (
     <>
       <div
         style={{
-          width: "40%",
+          width: matches ? 600 : "100vw",
           position: "absolute",
           left: 0,
           height: "100%",
           backgroundColor: palette.pageBG,
         }}
       >
-        <div style={{ marginTop: "25%" }}>
+        <div style={{ marginTop: 200 }}>
           <Title
             style={{
               color: palette.loginTitle,
-              textAlign: "center",
+              paddingLeft: 40,
               fontWeight: 600,
+              fontSize: 36,
             }}
             message="Sign in "
           />
           <Button
-            style={{ position: "absolute", left: 20, top: 20, backgroundColor: palette.pageBG, border: "none" }}
+            style={{
+              position: "absolute",
+              left: 20,
+              top: 20,
+              backgroundColor: palette.pageBG,
+              border: "none",
+            }}
             onClick={() => navigate("/")}
           >
             <img src={backIcon} alt="delete" />
           </Button>
-          <Form>
-            <Form.Group className="mb-2 mx-sm-4" controlId="formBasicEmail">
+          <Form
+            className="container-fluid"
+            style={{ paddingLeft: 40, paddingRight: 40, marginTop: 30 }}
+          >
+            <Form.Group className="mb-2" controlId="formBasicEmail">
               <Form.Label className="font-link">Email address</Form.Label>
               <Form.Control type="email" placeholder="name@address.com" />
             </Form.Group>
-            <Form.Group className="mb-2 mx-sm-4" controlId="formBasicPassword">
+            <Form.Group className="mb-2" controlId="formBasicPassword">
               <Form.Label className="font-link">Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            <div className="mx-sm-4 d-grid font-link" style={{ marginTop: 50 }}>
+            <div className="d-grid font-link" style={{ marginTop: 50 }}>
               <Button
                 type="submit"
                 style={{
@@ -73,15 +85,16 @@ export const SignIn: FC = () => {
       </div>
       <div
         style={{
-          width: "60%",
+          width: "calc(100% - 600px)",
           position: "absolute",
           right: 0,
           height: "100%",
           paddingLeft: 50,
-          backgroundColor: palette.pageBG,
+          backgroundColor: "red",
+          display: matches ? undefined : "none",
         }}
       >
-        <img
+        {/*<img TODO
           style={{
             maxWidth: "100%",
             objectFit: "cover",
@@ -89,7 +102,7 @@ export const SignIn: FC = () => {
           }}
           src={signUpImage}
           alt="signUp"
-        />
+        />*/}
       </div>
     </>
   );
