775786676e7a01c993fcd63fd4eaea35af5cbe24
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-06-13 21:12:53 +0200

    fix image

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { palette } from '../utils/colors';
-import loginImage from "../assets/loginImage.png";
+import loginImage from '../assets/loginImage.png';
 import useMediaQuery from '../hooks/useMediaQuery';
 import { SignupForm } from '../components/SignUp/SignupForm';
 import { Image } from 'react-bootstrap';
@@ -33,10 +33,12 @@ export const SignUp: FC = () => {
         }}
       >
         <Image
+          draggable={false}
+          fluid
           style={{
-            maxWidth: "100%",
-            objectFit: "cover",
-            height: "100%",
+            width: '100%',
+            minHeight: '100vh',
+            objectFit: 'cover',
           }}
           src={loginImage}
           alt="signUp"
