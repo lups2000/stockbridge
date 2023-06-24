3b60b4731c83d04a2ed63aef803eb6e41758891f
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-06-24 18:34:39 +0200

    carousel for top adverts

--- client/src/components/Home/TopAdverts.tsx
+++ client/src/components/Home/TopAdverts.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import { BodyText } from '../Text/BodyText';
 import { AdvertCard } from '../AdvertCard';
 
@@ -6,23 +8,39 @@ import { AdvertCard } from '../AdvertCard';
  * This component displays the top adverts from different categories, those that have been prioritized.
  */
 export const TopAdverts: FC = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1400 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1400, min: 1000 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 1000, min: 650},
+      items: 1,
+    },
+  };
+
   return (
     <div>
       <BodyText style={{ fontSize: 20, fontWeight: 600, paddingLeft: 25 }}>
         Top Adverts
       </BodyText>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          gap: 25,
-          paddingLeft: 25,
-          paddingRight: 25,
-        }}
-      >
-        <AdvertCard />
+      <div>
+        <Carousel responsive={responsive} infinite={true} showDots centerMode>
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+          <AdvertCard />
+        </Carousel>
       </div>
     </div>
   );
