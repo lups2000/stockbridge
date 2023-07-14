3ba223fbc2242740ceec17cbebe2d60e316b2000
Chaima Ghaddab <chaima.ghaddab@tum.de>
2023-07-14 17:52:05 +0200

    add categories icons

--- client/src/components/Home/TopCategories.tsx
+++ client/src/components/Home/TopCategories.tsx
@@ -1,15 +1,61 @@
-import React, { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { BodyText } from '../Text/BodyText';
 import { Image } from 'react-bootstrap';
-import icon from '../../assets/howWorks1.svg'; // stupid icon just for testing
-import { getPopularCategories } from '../../api/collections/advert';
+import { getPopularCategories, ProductCategory } from '../../api/collections/advert';
 import { useNavigate } from 'react-router-dom';
 import { FadeLoader } from 'react-spinners';
 import { palette } from '../../utils/colors';
-
+import { categories } from './icons';
 /**
  * This component displays the top categories, those with more adverts.
  */
+
+ export const mapIcon = (category: string) => {
+  switch (category) {
+    case ProductCategory.Apparel_And_Accessories:
+      return categories.apparel_and_accessories;
+    case ProductCategory.Electronics_And_Gadgets:
+      return categories.electronics;
+    case ProductCategory.Home_And_Kitchen:
+      return categories.kitchen; 
+    case ProductCategory.Furniture_And_Decor:
+      return categories.furniture;
+    case ProductCategory.Health_And_Beauty:
+      return categories.cosmetics;
+    case ProductCategory.Sports_And_Fitness:
+      return categories.fitness;
+    case ProductCategory.Books_And_Media:
+      return categories.books;
+    case ProductCategory.Toys_And_Games:
+      return categories.toys;
+    case ProductCategory.Automotive_Parts:
+      return categories.automotive;
+    case ProductCategory.Food_And_Beverages:
+      return categories.food; 
+    case ProductCategory.Office_Supplies:
+      return categories.office;
+    case ProductCategory.Tools_And_Hardware:
+      return categories.tools;
+    case ProductCategory.Pet_Supplies:
+      return categories.pet_supplies;
+    case ProductCategory.Babies_And_Kids_Products:
+      return categories.babies;
+    case ProductCategory.Jewelry_And_Accessories:
+      return categories.jewelry; 
+    case ProductCategory.Gardening_Supplies:
+      return categories.gardening; 
+    case ProductCategory.Art_And_Craft_Supplies:
+      return categories.arts_and_crafts; 
+    case ProductCategory.Musical_Instruments:
+      return categories.musical; 
+    case ProductCategory.Travel_And_Luggage:
+      return categories.travel; 
+    case ProductCategory.Flowers_And_Bouquets:
+        return categories.flowers;
+    default:
+      return categories.default_category;
+}
+}
 export const TopCategories: FC = () => {
   const [topCategories, setTopCategories] = useState<string[]>();
 
@@ -71,7 +117,7 @@ export const TopCategories: FC = () => {
                 }}
                 onClick={() => handleItemClick(cat)}
               >
-                <Image src={icon} alt="icon" width={100} height={100} />
+                <Image src={mapIcon(cat)} alt="icon" width={100} height={100} />
                 <span
                   style={{ paddingLeft: 10, paddingRight: 10, marginTop: 20 }}
                 >
