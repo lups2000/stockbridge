1942f3c9a469efb685766e76a511c5cae5dea67a
Matteo Luppi <ge92qes@mytum.de>
2023-07-19 13:48:12 +0200

    adjust alignement attributes

--- client/src/components/ProductOverview/ProductDetails.tsx
+++ client/src/components/ProductOverview/ProductDetails.tsx
@@ -3,7 +3,7 @@ import { BodyText } from '../Text/BodyText';
 import { ProductAttribute } from './ProductAttribute';
 import { Image } from 'react-bootstrap';
 import imagePlaceholder from '../../assets/product-placeholder.png';
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import { palette } from '../../utils/colors';
 import { categoryToAttributes, groupList } from './EditAdvertModal';
@@ -14,16 +14,28 @@ interface ProductDetailsProps {
 
 export const ProductDetails: FC<ProductDetailsProps> = (props) => {
   const { advert } = props;
-  const matches2 = useMediaQuery('(min-width: 850px)');
+  const [isColumn, setIsColumn] = useState(false);
+  const matches2 = useMediaQuery('(min-width: 990px)');
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsColumn(window.innerWidth <= 990);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
       <div
         style={{
           display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'start',
-          justifyContent: matches2 ? undefined : 'center',
+          flexDirection: isColumn ? 'column' : 'row',
+          alignItems: isColumn ? 'center' : 'start',
           gap: 50,
           marginLeft: 30,
           position: 'relative',
@@ -32,8 +44,9 @@ export const ProductDetails: FC<ProductDetailsProps> = (props) => {
         <div
           style={{
             display: 'flex',
-            flexDirection: 'row',
-            gap: 100,
+            flexDirection: isColumn ? 'column' : 'row',
+            alignItems: isColumn ? 'center' : undefined,
+            gap: 50,
           }}
         >
           <Image
@@ -48,7 +61,7 @@ export const ProductDetails: FC<ProductDetailsProps> = (props) => {
             }}
             src={advert?.imageurl ? advert?.imageurl : imagePlaceholder}
           />
-          <div>
+          <div style={{ width: !isColumn ? 700 : undefined }}>
             <div
               style={{
                 display: 'flex',
@@ -70,7 +83,7 @@ export const ProductDetails: FC<ProductDetailsProps> = (props) => {
                     fontWeight: 600,
                   }}
                 >
-                  {advert.productname ? advert.productname : 'N.S*'}
+                  {advert.productname ? advert.productname : 'N.S'}
                 </BodyText>
                 <div
                   style={{
@@ -92,7 +105,7 @@ export const ProductDetails: FC<ProductDetailsProps> = (props) => {
                       marginBottom: 0,
                     }}
                   >
-                    {advert.type ? advert.type : 'N.S*'}
+                    {advert.type ? advert.type : ''}
                   </BodyText>
                 </div>
                 {advert.prioritized ? (
@@ -137,43 +150,49 @@ export const ProductDetails: FC<ProductDetailsProps> = (props) => {
             </div>
             <ProductAttribute
               name="category"
-              value={advert.category ? advert.category : 'N.S*'}
+              value={advert.category ? advert.category : ''}
             />
             {groupList(categoryToAttributes(advert.category!) ?? [], 2).map(
-              (g) => {
+              (g, index) => {
                 return (
                   <div
+                    key={index}
                     style={{
                       display: 'flex',
-                      flexDirection: 'row',
-                      gap: '20%',
+                      flexDirection: isColumn ? 'column' : 'row',
                       alignItems: 'start',
                       justifyContent: 'start',
                       marginTop: '5%',
                       width: 'auto',
+                      gap: 50,
                     }}
                   >
-                    {' '}
                     {g.map((attribute) => {
                       const value = (advert as any)[attribute];
                       return (
                         attribute in advert &&
                         value && (
-                          <ProductAttribute
-                            name={attribute}
-                            value={
-                              [
-                                'purchaseDate',
-                                'expirationDate',
-                                'createdAt',
-                              ].includes(attribute)
-                                ? value.toString().substring(0, 10)
-                                : attribute === 'color'
-                                ? value.name
-                                : value
-                            }
-                            color={value.hex}
-                          ></ProductAttribute>
+                          <div
+                            style={{
+                              width: isColumn ? '100%' : '50%',
+                            }}
+                          >
+                            <ProductAttribute
+                              name={attribute}
+                              value={
+                                [
+                                  'purchaseDate',
+                                  'expirationDate',
+                                  'createdAt',
+                                ].includes(attribute)
+                                  ? value.toString().substring(0, 10)
+                                  : attribute === 'color'
+                                  ? value.name
+                                  : value
+                              }
+                              color={value.hex}
+                            ></ProductAttribute>
+                          </div>
                         )
                       );
                     })}
@@ -184,31 +203,32 @@ export const ProductDetails: FC<ProductDetailsProps> = (props) => {
             <div
               style={{
                 display: 'flex',
-                flexDirection: 'row',
-                gap: '20%',
+                flexDirection: isColumn ? 'column' : 'row',
+                gap: 50,
                 alignItems: 'start',
                 justifyContent: 'start',
                 marginTop: '5%',
                 width: 'auto',
               }}
             >
-              <ProductAttribute
-                name="quantity"
-                value={advert?.quantity}
-              ></ProductAttribute>
-              <ProductAttribute
-                name="price"
-                value={advert?.price}
-              ></ProductAttribute>
+              <div style={{ width: '50%' }}>
+                <ProductAttribute
+                  name="quantity"
+                  value={advert?.quantity}
+                  padding={'5px 25px'}
+                />
+              </div>
+              <div style={{ width: '50%' }}>
+                <ProductAttribute
+                  name="price"
+                  value={advert?.price}
+                  padding={'5px 25px'}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      {!advert.expirationDate || !advert.purchaseDate || !advert.color ? (
-        <BodyText style={{ position: 'absolute', bottom: 25, left: 15 }}>
-          *Not specified
-        </BodyText>
-      ) : undefined}
     </>
   );
 };
