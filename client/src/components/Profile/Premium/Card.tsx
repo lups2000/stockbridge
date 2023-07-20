b72fd3bd6b311d17e5884d788110e2b039fe3219
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-07-20 06:56:20 +0200

    fix

--- client/src/components/Profile/Premium/Card.tsx
+++ client/src/components/Profile/Premium/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card as BootStrapCard } from 'react-bootstrap';
 import { palette } from '../../../utils/colors';
 
@@ -12,20 +12,46 @@ export interface CardProps {
   disabled: boolean;
   mutedText?: string;
 }
+
 const Card = (props: CardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const isPlan = props.header.includes('Subscription');
+
+  const color = isPlan
+    ? props.header.includes('Basic')
+      ? palette.basicSub
+      : props.header.includes('Advanced')
+      ? palette.advancedSub
+      : palette.premiumSub
+    : palette.subSectionsBgAccent;
+
+  const paleColor = `${color}30`; // Here, '30' represents 30% opacity
+
   return (
     <BootStrapCard
       className={
         'shadow-sm p-0 m-auto text-center mb-5' +
         (props.outline ? '' : ' opacity-50')
       }
-      style={{ width: '18rem' }}
+      style={{
+        width: '18rem',
+        borderRadius: 20,
+        borderColor: isHovered ? color : undefined,
+        transition: 'transform 0.3s, box-shadow 0.3s', // Add transition for smooth effect
+        transform: isHovered ? 'scale(1.05)' : 'scale(1)', // Apply scaling effect on hover
+        boxShadow: isHovered ? '0 4px 8px rgba(0, 0, 0, 0.1)' : 'none', // Add shadow on hover
+        backgroundColor: props.buttonLabel.includes("Cancel") ? paleColor : undefined, // Use paleColor when outline is true
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <BootStrapCard.Header>
-        <h4>{props.header}</h4>
-      </BootStrapCard.Header>
       <BootStrapCard.Body>
-        <BootStrapCard.Title className="pricing-card-title">
+        <h4 style={{ marginBottom: 50 }}>{props.header}</h4>
+        <BootStrapCard.Title
+          className="pricing-card-title"
+          style={{ marginBottom: 50 }}
+        >
           {`${props.price}€`}
           <small className="text-muted"> {props.mutedText || ''}</small>
         </BootStrapCard.Title>
@@ -42,7 +68,8 @@ const Card = (props: CardProps) => {
           style={{
             width: '100%',
             border: 'none',
-            backgroundColor: palette.subSectionsBgAccent,
+            backgroundColor: color,
+            opacity: isHovered ? (isPlan ? 0.8 : 1.0) : 0.6,
             borderRadius: 15,
           }}
         >
